ad7f82e64e6d7c342b5d790b1d83590f4b9635dc
samsuteen-m <m.mohamedsamsuteen2508@gmail.com>
2026-06-10 12:19:33 +0530

    feat: configure app for deployment

--- src/app.module.ts
+++ src/app.module.ts
@@ -15,13 +15,12 @@ import { Patient } from './patient/patient.entity';
     }),
     TypeOrmModule.forRoot({
       type: 'postgres',
-      host: process.env.DB_HOST || 'localhost',
-      port: parseInt(process.env.DB_PORT || '5432'),
-      username: process.env.DB_USERNAME || 'postgres',
-      password: process.env.DB_PASSWORD || '123456',
-      database: process.env.DB_NAME || 'schedula',
+      url: process.env.DATABASE_URL,
       entities: [User, Doctor, Patient],
       synchronize: true,
+      ssl: {
+        rejectUnauthorized: false,
+      },
     }),
     AuthModule,
     DoctorModule,
